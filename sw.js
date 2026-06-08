609920f5d1d4f71008b6ada15fee2f326293a2d0
Anand-JEE <vatsanand81@gmail.com>
2026-06-09 05:19:28 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'jee-dashboard-v1';
+const CACHE = 'jee-dashboard-v2';
 const FILES = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', e => {
